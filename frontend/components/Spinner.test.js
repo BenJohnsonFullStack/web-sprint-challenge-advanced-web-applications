a6a94ab5089309ca3a640c40eebbca8416e8444f
Ben Johnson <monk.sound.design@gmail.com>
2023-07-26 14:33:51 -0400

    spinner test

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,17 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import Spinner from "./Spinner";
+import { render, screen } from "@testing-library/react";
+
+test("the spinner renders correctly when on", () => {
+  render(<Spinner spinnerOn={true} />);
+  const spinnerActive = screen.queryByText(/please wait/i);
+  expect(spinnerActive).not.toBeNull();
+});
+
+test("the spinner renders correctly when off", () => {
+  render(<Spinner spinnerOn={false} />);
+  const spinnerActive = screen.queryByText(/please wait/i);
+  expect(spinnerActive).toBeNull();
+});
